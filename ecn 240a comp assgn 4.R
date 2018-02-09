a1919712b2e99aedc808b99e9ce14ab5017c5cf5
Iwunze Ugo <iwunzeugo@campus-113-183.ucdavis.edu>
2018-02-08 17:06:49 -0800

    makes stand alone work

--- ecn 240a comp assgn 4.R
+++ ecn 240a comp assgn 4.R
@@ -1,13 +1,15 @@
 rm(list = ls())
 cat("\014")
+library(tidyverse)
 
 # consistency of beta
+set.seed(42069)
 f <- function(n) {
 n <- n
 b <- 4
 
 x <- runif(n,0,100)
-e <- rnorm(n,0,1)
+e <- rnorm(n,0,10)
 
 y <- b*x + e
 
@@ -28,18 +30,19 @@ g <- function(n) {
 N <- 200
 n <- c(1:N)
 
-ggplot() + geom_line(aes(n[-1],g(N)[-1]))
+ggplot() + geom_line(aes(n[-1],g(N)[-1])) + ylab("") + xlab("Number of observations") + labs(title = "The OLS Estimator is Consistent")
 
 # convergence in distribution
 
+set.seed(42069)
 f <- function(n) {
   n <- n
   b <- 4
   
   bhat <- c()
-  for (i in c(1:10)) {
+  for (i in c(1:1000)) {
   x <- runif(n,0,100)
-  e <- rnorm(n,0,1000)
+  e <- rnorm(n,0,10)
   y <- b*x + e
   
   bhat <- c(bhat, coef(lm(y~x))[2])
@@ -47,4 +50,4 @@ f <- function(n) {
   return(bhat)
 }
 
-ggplot() + geom_density(aes(f(100))) + geom_density(aes(f(1000))) + geom_density(aes(f(10000))) + geom_density(aes(f(100000)))
+ggplot() + geom_density(aes(f(100))) + geom_density(aes(f(1000))) + geom_density(aes(f(10000))) + xlab("") + ylab("Density") + labs(title = "The OLS Estimator Converges Approximately to a Normal Distribution")
